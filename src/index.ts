706864e72db950a96fae759365ed13c91ead8fa3
Robin Granda <robingranda@gmail.com>
2023-08-17 17:05:53 +0200

    fix generators

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { exercises } from './exercises/exercises';
+import { molecules } from './exercises/utils/molecularChemistry/molecule';
 
 const allScienceExercises = [...exercises];
+
+molecules.forEach((mol) => {
+  console.log({
+    name: mol.name,
+    formula: mol.formula,
+  });
+});
 
 export { allScienceExercises };

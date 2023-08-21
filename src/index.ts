6dd97729f1da1e0ec719812615070aeb0a4d0fea
Robin Granda <robingranda@gmail.com>
2023-08-21 09:42:41 +0200

    hide consoles

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { molecules } from './exercises/utils/molecularChemistry/molecule';
 
 const allScienceExercises = [...exercises];
 
-molecules.forEach((mol) => {
-  console.log({
-    name: mol.name,
-    formula: mol.formula,
-  });
-});
+// molecules.forEach((mol) => {
+//   console.log({
+//     name: mol.name,
+//     formula: mol.formula,
+//   });
+// });
 
 export { allScienceExercises };

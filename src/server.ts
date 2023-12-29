bc60a71b678244a736b68df2f697e868c0e428fd
Robin <robin@mathlive.fr>
2023-12-29 15:57:05 +0100

    fix exports

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import { exercises } from './exercises/exercises';
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+// import { exercises } from "./exercises";
+import * as Exercises from "./exercises";
+const exercises = Object.values(Exercises) as ScienceExercise<any>[];
+
+export { Exercises };
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import { ScienceExercise } from "./exercises/exercise";
+const jsonParser = bodyParser.json();
 
 const allExercises = [...exercises];
 
@@ -10,37 +17,74 @@ const runServer = () => {
   const app: Express = express();
   app.use(cors());
 
-  app.get('/', (req: Request, res: Response) => {
+  app.get("/", (req: Request, res: Response) => {
     res.json(allExercises);
   });
 
-  app.get('/exo', (req: Request, res: Response) => {
+  app.get("/exo", (req: Request, res: Response) => {
     const exoId = req.query.exoId;
-    const exo = allExercises.find((exo) => exo.id == exoId);
-    if (!exo) res.send('Exo not found');
+    const exoIndex = allExercises.findIndex((exo) => exo.id == exoId);
+    const exo = allExercises[exoIndex];
+    if (!exo) res.send("Exo not found");
     const questions = exo?.generator(10);
     res.json({
       exercise: exo,
       questions,
+      nextId: allExercises[(exoIndex + 1) % allExercises.length].id,
+      prevId:
+        allExercises[(exoIndex - 1 + allExercises.length) % allExercises.length]
+          .id,
     });
   });
 
-  app.get('/qcmExo', (req: Request, res: Response) => {
+  app.get("/qcmExo", (req: Request, res: Response) => {
     const exoId = req.query.exoId;
-    const exo = allExercises.find((exo) => exo.id == exoId);
-    if (!exo) res.send('Exo not found');
+    const exoIndex = allExercises.findIndex((exo) => exo.id == exoId);
+    const exo = allExercises[exoIndex];
+
+    if (!exo) res.send("Exo not found");
     const questions = exo?.generator(10);
-    console.log(questions);
     const populatedQuestions = questions?.map((q) => {
-      return { ...q, propositions: q.getPropositions?.(4) };
+      return {
+        ...q,
+        propositions: exo.getPropositions?.(4, {
+          answer: q.answer,
+          ...q.identifiers,
+        }),
+      };
     });
     res.json({
       exercise: exo,
       questions: populatedQuestions,
+
+      nextId: allExercises[(exoIndex + 1) % allExercises.length].id,
+      prevId:
+        allExercises[(exoIndex - 1 + allExercises.length) % allExercises.length]
+          .id,
     });
   });
 
-  app.listen('5000', () => {
+  app.post("/vea", jsonParser, (req: Request, res: Response) => {
+    const exoId = req.query.exoId;
+
+    const { ans, veaProps } = req.body;
+    const exoIndex = allExercises.findIndex((exo) => exo.id == exoId);
+    const exo = allExercises[exoIndex];
+    if (!exo) {
+      res.send("Exo not found");
+      return;
+    }
+    if (!exo.isAnswerValid) {
+      res.send("No VEA implemented");
+      return;
+    }
+    const result = exo.isAnswerValid(ans as string, veaProps) ?? false;
+    res.json({
+      result,
+    });
+  });
+
+  app.listen("5000", () => {
     console.log(`[server]: Server is running at http://localhost:5000`);
   });
 };

f46ef010d000c42977e294d1700415348fec92bf
Robin <robin@mathlive.fr>
2023-12-29 15:58:13 +0100

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
-import { ScienceExercise, ScienceSection } from './exercises/exercise';
-import { exercises } from './exercises/exercises';
+import * as Exercises from "./exercises";
+const scienceExercises = Object.values(Exercises);
 
-const allScienceExercises = [...exercises];
-
-const getAllScienceExercisesBySection = () => {
-  const data: { section: ScienceSection; exos: ScienceExercise[] }[] = [];
-
-  allScienceExercises.forEach((exo) => {
-    const sectionsData = data.filter((el) => exo.sections.includes(el.section));
-    if (!sectionsData.length) {
-      exo.sections.forEach((section) =>
-        data.push({
-          section,
-          exos: [exo],
-        }),
-      );
-    } else {
-      sectionsData.forEach((sectionData) => {
-        data.find((d) => d.section === sectionData.section)?.exos.push(exo);
-      });
-    }
-  });
-  return data;
-};
-
-export { allScienceExercises, getAllScienceExercisesBySection };
+export { scienceExercises };
